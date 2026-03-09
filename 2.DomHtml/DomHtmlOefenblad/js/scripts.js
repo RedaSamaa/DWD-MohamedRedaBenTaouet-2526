141856dc0b5af6248a0f455f26b0834f2b7381bd
RedaSamaa <reda.bentaouet@student.odisee.be>
2026-03-09 18:29:02 +0100

    Huiswerk af

---
+++ 2.DomHtml/DomHtmlOefenblad/js/scripts.js
@@ -0,0 +1,89 @@
+// === Oefening 1: querySelector() ===
+{
+	console.log('\n%c=== querySelector ===', 'color: #0c0');
+	const hdrTekoop = document.querySelector('#ex1-html h3');
+   console.log(hdrTekoop);
+
+   const vaasImg = document.querySelector('#ex1-html #vaas');
+   console.log(vaasImg);
+
+   const lijstje = document.querySelector('#ex1-html .kenmerken');
+   console.log(lijstje);
+
+   const nthChildList = document.querySelector('#ex1-html .kenmerken li:nth-child(2)')
+   console.log(nthChildList);
+}
+
+// === Oefening 2: querySelectorAll() ===
+{
+	console.log('\n%c=== querySelectorAll ===', 'color: #0c0');
+   // ... vul hier aan
+
+}
+
+// === Oefening 3: addEventListener() ===
+{
+	console.log('\n%c=== addEventListener() ===', 'color: #0c0');
+
+   // declaraties
+   // ... vul hier aan
+
+	// event handler functies
+   // ... vul hier aan
+
+	// events koppelen
+   // ... vul hier aan
+}
+
+// === Oefening 4: innerHTML en innerText ===
+{
+	// DOBBELSTEEN OEFENING
+
+   // ... vul hier aan
+
+   // AANTAL TEKENS OEFENING
+
+   // ... vul hier aan
+}
+
+// === Oefening 5: HTML-attributen lezen en schrijven ===
+{
+   // ... vul hier aan
+
+}
+
+// === Oefening 6: validatie tekstinvoer ===
+{
+   // ALLEEN LETTERS EN SPATIES OEFENING
+
+   // ... vul hier aan
+
+   // WACHWOORD VALIDATIE OEFENING
+
+   // ... vul hier aan
+
+}
+
+// === Oefening 7: e.preventDefault() ===
+{
+   // ... vul hier aan
+
+}
+
+// === Oefening 8: e.target ===
+{
+   // ... vul hier aan
+
+}
+
+// === Oefening 9: event bubbling ===
+{
+   // ... vul hier aan
+
+}
+
+// === Oefening 10: formvalidatie ===
+{
+   // ... vul hier aan
+
+}
